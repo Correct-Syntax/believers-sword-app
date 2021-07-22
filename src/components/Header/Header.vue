4d1b53b81f58d1ff39f7db3b71a018e5fd071cbf
Jenuel Ganawed <jenuelganawed936@gmail.com>
2021-07-23 02:18:09 +0800

    setup removed other css styles, going to use tailwind css instead
    - reason to use tialwind css. If projects becomes bigger. Edditing Css would be less trouble.

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -1,32 +1,45 @@
 <template>
-    <div class="navigation-header">
+    <div
+        class="
+            flex
+            justify-between
+            w-full
+            fixed
+            items-center
+            bg-gray-50 bg-opacity-70
+            z-[90]
+            h-[var(--nav-height)]
+            shadow-md
+            backdrop-filter backdrop-blur-sm
+        "
+    >
         <div class="nav-logo">
             <img
                 src="./../../assets/images/logo.png"
                 alt="logo"
-                class="h-[90px]"
+                class="h-[80px] pl-[20px]"
             />
         </div>
-        <div class="navigation">
-            <ul>
-                <li>
+        <div class="h-full">
+            <ul class="flex h-full p-[5px] gap-[10px]">
+                <li class="h-full flex justify-center items-center w-[70px]">
                     <router-link to="/bible">
                         <button>Bible</button>
                     </router-link>
                 </li>
-                <li>
+                <li class="h-full flex justify-center items-center w-[70px]">
                     <router-link to="/sermons">
                         <button>Sermons</button>
                     </router-link>
                 </li>
-                <li>
+                <li class="h-full flex justify-center items-center w-[70px]">
                     <router-link to="/store">
                         <button>Store</button>
                     </router-link>
                 </li>
             </ul>
         </div>
-        <div class="nav-account">
+        <div class="pr-[20px]">
             <NSpace align="center">
                 <div>Account</div>
                 <MoreButton />
@@ -35,4 +48,3 @@
     </div>
 </template>
 <script lang="ts" src="./Header.ts" />
-<style lang="scss" src="./Header.scss" />
