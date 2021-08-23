f68b6757201aa59b613db9d89752fd9fa02d8170
Jenuel Ganawed <jenuelganawed936@gmail.com>
2021-08-23 12:02:09 +0800

    updates

--- src/components/TitleBar/TitleBar.vue
+++ src/components/TitleBar/TitleBar.vue
@@ -1,57 +1,21 @@
 <template>
-    <header
-        id="titlebar"
-        class="
-            dark:bg-gray-800
-            dark:text-gray-100
-            text-size-[12px]
-            flex
-            w-[100%]
-            justify-between
-            whitespace-nowrap
-            h-[var(--header-height)]
-        "
-    >
+    <header id="titlebar" class="dark:bg-gray-800 dark:text-gray-100 text-size-[12px] flex w-[100%] justify-between whitespace-nowrap h-[var(--header-height)]">
         <div class="p-[10px]">Believers Sword</div>
-        <div
-            id="drag-region"
-            class="w-[100%] p-[10px]"
-            style="cursor: move"
-        ></div>
+        <div id="drag-region" class="w-[100%] p-[10px]" style="cursor: move"></div>
         <div class="flex">
-            <div class="p-[10px] hover:bg-gray-600" @click="minimize()">
-                <svg
-                    class="fill-gray-50"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="15"
-                    height="15"
-                    viewBox="0 0 24 24"
-                >
+            <div id="minimizeButton" class="p-[10px] hover:bg-gray-600" @click="minimize()">
+                <svg class="fill-gray-50" xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24">
                     <path d="M5 11h14v2H5z"></path>
                 </svg>
             </div>
-            <div class="p-[10px] hover:bg-gray-600" @click="maximize()">
-                <svg
-                    class="fill-gray-50"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="15"
-                    height="15"
-                    viewBox="0 0 24 24"
-                >
+            <div id="maximizeButton" class="p-[10px] hover:bg-gray-600" @click="maximize()">
+                <svg class="fill-gray-50" xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24">
                     <path d="M5 12H3v9h9v-2H5zm7-7h7v7h2V3h-9z"></path>
                 </svg>
             </div>
-            <div class="p-[10px] hover:bg-red-600" @click="close()">
-                <svg
-                    class="fill-gray-50"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="15"
-                    height="15"
-                    viewBox="0 0 24 24"
-                >
-                    <path
-                        d="m16.192 6.344-4.243 4.242-4.242-4.242-1.414 1.414L10.535 12l-4.242 4.242 1.414 1.414 4.242-4.242 4.243 4.242 1.414-1.414L13.364 12l4.242-4.242z"
-                    ></path>
+            <div id="closeButton" class="p-[10px] hover:bg-red-600" @click="close()">
+                <svg class="fill-gray-50" xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24">
+                    <path d="m16.192 6.344-4.243 4.242-4.242-4.242-1.414 1.414L10.535 12l-4.242 4.242 1.414 1.414 4.242-4.242 4.243 4.242 1.414-1.414L13.364 12l4.242-4.242z"></path>
                 </svg>
             </div>
         </div>
@@ -59,19 +23,19 @@
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
-import { ipcRenderer } from "electron"; 
+import { ipcRenderer } from "electron";
 
 export default defineComponent({
     setup() {
         return {
             minimize: () => {
-                ipcRenderer.send('minimizeWindow');
+                ipcRenderer.send("minimizeWindow");
             },
             maximize: () => {
-                ipcRenderer.send('maximizeWindow');
+                ipcRenderer.send("maximizeWindow");
             },
             close: () => {
-                ipcRenderer.send('closeWindow');
+                ipcRenderer.send("closeWindow");
             },
         };
     },
@@ -81,5 +45,10 @@ export default defineComponent({
 <style lang="scss">
 #drag-region {
     -webkit-app-region: drag;
+}
+
+#minimizeButton, #maximizeButton, #closeButton {
+    -webkit-app-region: no-drag;
+    cursor: pointer !important;
 }
 </style>
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="main-container flex h-[100%] flex-col px-10px pb-10px">
         <div id="main-container-wrapper" class="h-[100%] w-[100%] relative">
-            <div id="leftSide" class="w-[var(--left-width)] border border-red-600 h-[100%] resize-x absolute top-0">
+            <div id="leftSide" class="w-[var(--left-width)] h-[100%] bg-light-50 bg-opacity-10 resize-x absolute top-0">
                 <LeftSideBar />
                 <div id="dragbar" class="right-0 w-5px opacity-0 bg-light-50 h-[100%] bg-opacity-30 active:opacity-100 duration-300 absolute top-0" style="cursor: col-resize"></div>
             </div>
-            <div id="mainWindow" class="min-w-300px border border-blue-500 absolute h-[100%] right-0">
+            <div id="mainWindow" class="min-w-300px absolute h-[100%] right-0">
                 <router-view></router-view>
             </div>
         </div>
