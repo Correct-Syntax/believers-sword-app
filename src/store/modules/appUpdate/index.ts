e9bf0c82a902686727551a9fe354de04d53569cb
Jenuel Ganawed <jenuelganawed936@gmail.com>
2021-09-11 18:12:48 +0800

    updates

--- src/store/modules/appUpdate/index.ts
+++ src/store/modules/appUpdate/index.ts
@@ -2,7 +2,7 @@ export const appUpdate = {
     state: {
         updateAvailable: false,
         error: {},
-        downloadProgress: null,
+        downloadProgress: false,
         updatedDownloaded: false
     },
     mutations: {
