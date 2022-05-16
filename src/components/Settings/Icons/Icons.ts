32e7e8a05a4b6db7f8c09d9d82a9b35fd3202efc
Jenuel Ganawed <jenuelganawed936@gmail.com>
2022-05-16 08:29:58 +0800

    Create Icons.ts

---
+++ src/components/Settings/Icons/Icons.ts
@@ -0,0 +1,10 @@
+import { Book, DirectionFork, GameConsole, ListDropdown, MessageQueue } from "@vicons/carbon"
+import { Book24Regular } from "@vicons/fluent"
+
+export const FluentIcons = {
+    "BibleBookIcon": Book,
+    "SermonsIcon": MessageQueue,
+    "DirectionIcon": DirectionFork,
+    "PrayerListIcon": ListDropdown,
+    "GameIcon": GameConsole
+};
