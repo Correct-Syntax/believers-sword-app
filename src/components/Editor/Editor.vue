2b21e5792e3217d494a5c99f5aa5ad0c40712a2d
Jenuel Ganawed <jenuelganawed936@gmail.com>
2022-08-23 21:01:07 +0800

    Update

--- src/components/Editor/Editor.vue
+++ src/components/Editor/Editor.vue
@@ -20,7 +20,7 @@ import {
 import { extensionsUsed } from "./editor-options";
 // import { watch } from "vue";
 const emit = defineEmits(['update:modelValue']);
-const props = defineProps(['modelValue']);
+// const props = defineProps(['modelValue']);
 const editor: any = useEditor({
     content: "",
     extensions: extensionsUsed,
